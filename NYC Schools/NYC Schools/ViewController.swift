fbf8c8eec39b7c30a2c1b58751f1467f078d098b
Igor Dudenkov <igor.dudenkov@revolut.com>
2019-04-12 17:09:37 +0100

    Add an initial project

---
+++ NYC Schools/NYC Schools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYC Schools
+//
+//  Created by Igor Dudenkov on 12/04/2019.
+//  Copyright © 2019 Igor Dudenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
